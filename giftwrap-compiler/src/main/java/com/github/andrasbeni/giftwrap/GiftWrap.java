e3ea0b14e928887c3954906154cea9f4f656f830
Andras Beni <andrasbeni@users.noreply.github.com>
2016-03-17 07:52:57 +0100

    javadoc

--- giftwrap-compiler/src/main/java/com/github/andrasbeni/giftwrap/GiftWrap.java
+++ giftwrap-compiler/src/main/java/com/github/andrasbeni/giftwrap/GiftWrap.java
@@ -5,6 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * Giftwrap compiler module creates decorators for 
+ * classes and interfaces annotated with <code>GiftWrap</code>. 
+ */
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
 public @interface  GiftWrap {
